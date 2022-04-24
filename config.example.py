9103cec765dd0a146319d3c61ad5ea8f65e9f531
efe3535 <a.efe.akyazi@gmail.com>
2022-04-25 00:01:03 +0300

    show thumbnails - spotify

--- config.example.py
+++ config.example.py
@@ -10,8 +10,9 @@ envpath = 'YOUR ENV FILE PATH'
 persist_wallpapers = True # False by default.
 persistent_wallpapers_dir = f'/home/{getlogin()}/.walyrics/' # if you get an error saying .walyrics folder is not found then simply create it using mkdir.
 
-fontsize = 14
-offset_x = 25
-offset_y = 25
-randlimit = 1000
+thumbsize = 150 # Thumbnail size
+fontsize = 14 # Font size for lyrics
+offset_x = 25 # X axis offset
+offset_y = 25 # Y axis offset
+randlimit = 1000 # Random limit for temporary pictures
 charlimit = (1366 // fontsize) * (768 // fontsize) # You can replace 1366 with your screen width and 768 with your screen height.
