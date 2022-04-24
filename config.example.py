dde6afb4335c828f6335aa537817131cfa51a5d4
efe3535 <a.efe.akyazi@gmail.com>
2022-04-24 22:29:39 +0300

    add envpath example in config.example.py

--- config.example.py
+++ config.example.py
@@ -2,4 +2,4 @@ wallpaper = "/home/enginar/Pictures/wallpapers/material.png"
 fontcolor = "#4b526d"
 fontpath = '/usr/share/fonts/TTF/iosevka-medium.ttf'
 fontsize = 14
-
+envpath = '/home/enginar/blabla/.env'
