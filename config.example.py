c51957c98841f3721bada56a7b6130763886f851
efe3535 <a.efe.akyazi@gmail.com>
2022-04-24 23:30:44 +0300

    update example config accordingly to new walyrics features

--- config.example.py
+++ config.example.py
@@ -1,11 +1,17 @@
-wallpaper = "YOUR_WALLPAPER_PATH"
+from os import getlogin
+
+wallpaper = "YOUR WALLPAPER DIRECTORY"
 
 fontcolor = "#4b526d"
 
 fontpath = '/usr/share/fonts/TTF/iosevka-medium.ttf'
-envpath = 'YOUR_ENV_PATH'
+envpath = 'YOUR ENV FILE PATH'
+
+persist_wallpapers = True # False by default.
+persistent_wallpapers_dir = f'/home/{getlogin()}/.walyrics/' # if you get an error saying .walyrics folder is not found then simply create it using mkdir.
 
 fontsize = 14
 offset_x = 25
 offset_y = 25
 randlimit = 1000
+charlimit = (1366 // fontsize) * (768 // fontsize) # You can replace 1366 with your screen width and 768 with your screen height.
