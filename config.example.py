e291237813d55d761666cc6afeb11ed7343d10a3
efe3535 <a.efe.akyazi@gmail.com>
2022-04-26 00:17:33 +0300

    update config.example.py

--- config.example.py
+++ config.example.py
@@ -7,7 +7,7 @@ fontcolor = "#4b526d" # text color
 fontpath = '/usr/share/fonts/TTF/iosevka-medium.ttf' # your font's path
 envpath = 'YOUR ENV FILE PATH' # your env file path: required for genius lyrics api
 
-persist_wallpapers = False # False by default.
+persist_wallpapers = False # False by default. ENABLE IF YOU WILL USE A DE RATHER THAN FEH.
 persistent_wallpapers_dir = f'/home/{getlogin()}/.walyrics/' # if you get an error saying .walyrics folder is not found then simply create it using mkdir.
 
 marquee = True # True by default
@@ -18,6 +18,8 @@ show_thumbs = True # Show thumbs by default
 thumbsize = 150 # Thumbnail size
 
 fontsize = 14 # Font size for lyrics
+
+cinnamon = False # Cinnamon DE support
 
 offset_x = 25 # X axis offset
 offset_y = 25 # Y axis offset
