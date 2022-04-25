2f56253429c0fb46b88c13aff163021a43ebc35e
efe3535 <a.efe.akyazi@gmail.com>
2022-04-25 17:57:50 +0300

    Add marquee

--- config.example.py
+++ config.example.py
@@ -10,10 +10,17 @@ envpath = 'YOUR ENV FILE PATH' # your env file path: required for genius lyrics 
 persist_wallpapers = False # False by default.
 persistent_wallpapers_dir = f'/home/{getlogin()}/.walyrics/' # if you get an error saying .walyrics folder is not found then simply create it using mkdir.
 
+marquee = True # True by default
+marquee_pixels = 15 # pixels to move lyrics up
+marquee_sleep = 0.7 # seconds to sleep
+
 show_thumbs = True # Show thumbs by default
 thumbsize = 150 # Thumbnail size
+
 fontsize = 14 # Font size for lyrics
+
 offset_x = 25 # X axis offset
 offset_y = 25 # Y axis offset
+
 randlimit = 1000 # Random limit for temporary pictures
 charlimit = (1366 // fontsize) * (768 // fontsize) # You can replace 1366 with your screen width and 768 with your screen height.
